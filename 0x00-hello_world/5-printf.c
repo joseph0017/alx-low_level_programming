fb14e6c4dd590f6e332dd1c0fb7ea6d5060d9cc0
joseph0017 <josephogiku96@gmail.com>
2022-09-08 05:32:30 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: 'to display a string using the puts method'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
