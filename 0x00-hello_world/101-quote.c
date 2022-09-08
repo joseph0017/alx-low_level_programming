7be56a9213357aae691489a37af47dbba5dd931e
joseph0017 <josephogiku96@gmail.com>
2022-09-08 06:44:39 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: 'to display a string using the sprintf method'
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return 0;
+}
