b46986a0671b7856dd016821152c5832d502b222
joseph0017 <josephogiku96@gmail.com>
2022-09-09 13:36:38 -0700

    initialcommit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: 'inequalities'
+ * Return: Always an Integer(Success)
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = '0'; numbers <= '9', numbers = numbers + 1)
+	{
+		putchar(numbers);
+		if (numbers < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
