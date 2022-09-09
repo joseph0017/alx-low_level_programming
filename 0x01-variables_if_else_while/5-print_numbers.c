3f1643a9a2d6fc02ca2a25c6cc524db0cd087fc7
joseph0017 <josephogiku96@gmail.com>
2022-09-09 12:01:41 -0700

    initialcommit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ * description: 'base 10'
+ * return: numbers
+ */
+
+int main(void)
+{
+	int number;
+	
+	for (number = 0; number <= 9; number++)
+	{
+		printf('%d', number);
+	}
+
+	putchar('\n);
+	return (0);
+}
