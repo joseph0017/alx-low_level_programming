918aac851ea47d7fc44851b23c4ebbfc38d81385
joseph0017 <josephogiku96@gmail.com>
2022-09-09 11:49:49 -0700

    initialcommit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ * description: 'not e and q'
+ * Return: something
+ */
+
+int main(void)
+{
+	char alphs, not_e, not_q;
+
+	not_e = 'e';
+	not_q = 'q';
+
+	for (alphs = 'a'; alphs <= 'q'; alphs = alphs + 1)
+	{
+		if (alphs != not_e && alphs != not_q)
+		{
+			putchar(alphs);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
