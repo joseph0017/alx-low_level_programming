91f71c1776463f6c620e74634921c4360a0f06ab
joseph0017 <josephogiku96@gmail.com>
2022-09-09 12:08:43 -0700

    initialcommit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,18 +5,17 @@
 /**
  * main - Entry Point
  * description: 'base 10'
- * return: numbers
+ * Return: numbers
  */
 
 int main(void)
 {
 	int number;
-	
 	for (number = 0; number <= 9; number++)
 	{
 		printf("%d", number);
 	}
-
+	
 	putchar('\n');
 
 	return (0);
