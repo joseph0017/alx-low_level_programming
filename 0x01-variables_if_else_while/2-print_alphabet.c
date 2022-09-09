d77c058e1ef1a396a9758e2c3f804f37a0ae3b19
joseph0017 <josephogiku96@gmail.com>
2022-09-09 09:09:38 -0700

    initialcommit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main = Entry Point
+ * description: 'alphabets'
+ * Return: alphabets 
+ */
+int main(void)
+{
+	char lower_alphs;
+
+	for (lower_alphs = 'a'; lower_alphs <= 'z'; lower_alphs +=1 )
+	{
+		putchar(lower_alphs);
+		putchar('\n');
+	}
+	return (0);
+}
