bb8e063f541250d79dfc7d8fe1ee185b01372726
joseph0017 <josephogiku96@gmail.com>
2022-09-09 08:00:43 -0700

    initialcommit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ * Description: 'inequalities'
+ * Return: Always an Integer(Success)
+ */
+
+int main(void)
+{
+	int n, digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	digit = n % 10;
+
+	if (digit > 5)
+	{
+		printf("%d and is greater than 5\n", digit);
+	}
+	else if (digit ==0)
+	{
+		printf("%d and is 0", digit);
+	}
+	else if (digit < 6)
+	{
+		printf("%d and is less than 6 and not 0");
+	}
+	return (0);
+}
