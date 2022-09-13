d0ed96a0417ef2e8c497250a48ac5740729b3a30
joseph0017 <josephogiku96@gmail.com>
2022-09-13 07:32:52 -0700

    new commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,23 +2,28 @@
 #include "main.h"
 
 /**
+ * print_alphabet - prints all the alphabets in lower case
+ * Return: alphabets in lower case
+ */
+
+void print_alphabet(void)
+{
+	char alphs;
+
+	for (alphs = 'a'; alphs <= 'z'; alphs = alphs + 1)
+	{
+		putchar(alphs);
+	}
+	putchar('\n');
+}
+
+/**
  * main - entry
  * Return: lower alphabets
  */
 
 int main(void)
 {
-	void print_alphabet(void)
-	{
-		char alphs;
-
-		for (alphs = 'a'; alphs <= 'z'; alphs = alphs + 1)
-		{
-			putchar(alphs);
-		}
-		putchar('\n');
-	}
-
 	print_alphabet();
 
 	return (0);
