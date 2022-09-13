541a5086837eb76a25b19fbf06b0f2ca61a48e5a
joseph0017 <josephogiku96@gmail.com>
2022-09-13 13:20:37 -0700

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if its both uppercase and lowercase and gives aa value of 1
+ * Return: always 1 success
+ */
+
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower = lower +1)
+	{
+		for (upper = 'A'; upper = 'Z'; upper = upper +1)
+		{
+			if (c == lower || c == upper)
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+}
