ebfe732ebcb478f18416379ffee8ad320e86948e
joseph0017 <josephogiku96@gmail.com>
2022-09-13 21:39:45 -0700

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - simply addition of two numbers
+ * @a: first parameter
+ * @b: second parametr
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,7 @@ int print_sign(int n);
 int _abs(int c);
 int print_last_digit(int i);
 void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
 #endif /* _MAIN_H_ */
