9294268c37d4918712b7c077162b2c542aef9f3c
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2022-09-14 07:20:29 -0700

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 
 
-#endif /* _MAIN_H_ */
+#endif /* MAIN_H */
