db4107b70b4a331c1622848bb6ea2bd642cf114f
joseph0017 <josephogiku96@gmail.com>
2022-09-15 07:23:10 -0700

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,7 @@ void more_numbers(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num = num + 1)
+	for (num1 = 0; num1 < 10; num1 = num1 + 1)
 	{
 		for (num2 = 0; num2 <= 14; num2 = num2 + 1)
 		{
