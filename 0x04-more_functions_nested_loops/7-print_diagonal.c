862a00151b63e116557cc929d73f0184fe54cabc
joseph0017 <josephogiku96@gmail.com>
2022-09-15 08:42:41 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - goes in diagonal like a bishop
+ * @n: diagonal parameter
+ * Return: diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int line;
+	int space;
+
+	for (line = 0; line < n; line++)
+	{
+		for (space = 0; space < line; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
