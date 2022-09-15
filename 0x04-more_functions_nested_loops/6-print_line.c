e067aa721515732106bcc478d8eefc0a35d622cc
joseph0017 <josephogiku96@gmail.com>
2022-09-15 07:59:36 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints underscores
+ * @n: number of underscores
+ * Return: values
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	for (line = 0; line < n; line = line + 1)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
