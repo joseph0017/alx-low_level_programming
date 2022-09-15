11c144954b824a246dccb09a05cf710632a5e76d
joseph0017 <josephogiku96@gmail.com>
2022-09-15 09:20:59 -0700

    more functions

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,23 @@ void print_square(int size)
 	int hash1;
 	int hash2;
 
-	for (hash1 = 0; hash1 < size; hash1++)
+	if (size > 0)
 	{
 
-		for (hash2 = 1; hash2 < size; hash2++)
+		for (hash1 = 0; hash1 < size; hash1++)
 		{
-			_putchar('#');
-		}
 
-		_putchar('#');
+			for (hash2 = 1; hash2 < size; hash2++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else 
+	{
 		_putchar('\n');
 	}
 }
