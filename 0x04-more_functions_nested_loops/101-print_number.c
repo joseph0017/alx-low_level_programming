094adf9592e8f0afd2e81a058ee065add99c6b26
joseph0017 <josephogiku96@gmail.com>
2022-09-15 14:11:49 -0700

    more functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 
@@ -19,5 +19,5 @@ void print_number(int n)
 		print_number(n / 10);
 	}
 
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
