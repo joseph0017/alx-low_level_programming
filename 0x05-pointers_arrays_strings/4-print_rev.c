852b5934beba697c9655d052988fa5bb51e1fadc
joseph0017 <josephogiku96@gmail.com>
2022-09-19 15:15:51 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in revers
+ * @s: argument
+ * Return: strings
+ */
+
+void print_rev(char *s)
+{
+	int j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		j--;
+	}
+	_putchar('\n');
+}
