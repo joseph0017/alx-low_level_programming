d09f32c45b2e94c9d72689124154bae88361882a
joseph0017 <josephogiku96@gmail.com>
2022-09-19 15:36:21 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#inlcude "main.h"
+
+/**
+ * rev_string - function that reverses strings
+ * @s - argument
+ * Return: string
+ */
+
+void rev_string(char *s)
+{
+	int a, b, c, tni;
+
+
+	a = 0;
+	while (s[a] != '\0')	
+	{
+		a++;
+	}
+
+	c = 0;
+	b = a - 1;
+
+	while (c < b)
+	{
+		tni = s[c];
+		s[c] = s[b];
+		s[b] = tni;
+		c++;
+		b--;
+	}
+	
+}
