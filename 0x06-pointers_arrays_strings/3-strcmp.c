ca50771f6bc6bf00a93e702ea1d171cc3987259e
joseph0017 <josephogiku96@gmail.com>
2022-09-21 23:33:36 -0700

    more pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	while (s1 && s2)
 	{
-		if (s1 == s2)
+		if (s1 != s2)
 		{
-			s1++;
-			s2++;
+			return (*s1 - *s2);
 		}
-		return (s1 - s2);
+		s1 = s1 + 1;
+		s2 = s2 + 1;
 	}
 	return (s1 - s2);
 }
