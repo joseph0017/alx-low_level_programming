889314778630161216a2c1d337e5edd18c26f7c2
joseph0017 <josephogiku96@gmail.com>
2022-09-22 08:54:12 -0700

    more pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *upper);
 char *cap_string(char *);
-char *leet(char *);
+char *leet(char *c);
+void print_number(int n);
+int _putchar(char c);
