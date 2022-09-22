8c0fbbdd2ab53ed9e0bdb42f34af6bd9b31c18aa
joseph0017 <josephogiku96@gmail.com>
2022-09-22 02:31:17 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: left string
+ * @s2: right string
+ * Return: 0 if equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+	{
+		return (*s1 - *s2);
+	}
+}
