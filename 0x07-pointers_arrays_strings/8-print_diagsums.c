aadca71c882b5af1ceda5b1d86660ed27365a48c
joseph0017 <josephogiku96@gmail.com>
2022-09-27 15:00:19 -0700

    another pointer

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix 
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: argument
  * @size: argument
  * Return: diagonal
