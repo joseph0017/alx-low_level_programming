26c9ca1acf74edf627c6081b89f1aab7034544f8
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2022-10-01 09:03:17 -0700

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute integer
+ * @c: an argument
+ * Return: 0
+ */
+
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
