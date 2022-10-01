a90f4e0f3c3820d700093260800978b620e3815d
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2022-10-01 09:06:33 -0700

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if it is a digit
+ * @c: parameter
+ * Return: 1
+ */
+
+int _isdigit(int c)
+{
+	char digit;
+
+	int num = 0;
+
+	for (digit = '0'; digit <= '9'; digit = digit + 1)
+	{
+		if (digit == c)
+		{
+			num = 1;
+		}
+	}
+
+	return (num);
+}
