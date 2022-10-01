d5a20a29da20cc91511d963206689cf7695ccdb1
joseph0017 <josephogiku96@gmail.com>
2022-10-01 06:11:54 -0700

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 always succes
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, multiply;
+
+	if(argc!=3)
+	{
+		printf("sorry, only two values allowed\n");
+		return (-1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	multiply = a * b;
+
+	printf("%d\n", multiply);
+	return (0);
+}
