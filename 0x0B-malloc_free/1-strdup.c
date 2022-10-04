531f9413e7388ed1c2b8bf0a7e82b408f9552e08
joseph0017 <josephogiku96@gmail.com>
2022-10-04 04:46:32 -0700

    mallocs

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,5 +8,25 @@
 
 char *_strdup(char *str)
 {
-	
+	int org_size;
+	static char *dup;
+	char *dup_offset;
+
+	org_size = strlen(str);
+	dup = (char *) malloc(sizeof(char) * org_size + 1);
+
+	if (dup == NULL)
+		return ((char *) NULL);
+
+	dup_offset = dup;
+	while (*str)
+	{
+		*dup_offset = *str;
+		dup_offset++;
+		str++;
+	}
+	*dup_offset = '\0';
+
+	return (dup);
+
 }
