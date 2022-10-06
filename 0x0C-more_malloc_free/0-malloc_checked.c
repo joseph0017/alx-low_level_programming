cdf83d8a6677550f24808efa470b32e67bdc2b16
joseph0017 <josephogiku96@gmail.com>
2022-10-06 09:16:20 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: argument
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+		exit(98);
+
+	return(i);
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two positive numbers
+ * @argc: arguments
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int, num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else if
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul - num1 * num2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
