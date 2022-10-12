7b2e420417950b4cdd76e7640f9e48be71062cc4
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2022-10-12 01:05:21 -0700

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>                                                                                                         
-                                                                                                                            
-/**                                                                                                                         
- * array_iterator - function given as a parameter on each element of an array.                                              
- * @array: A pointer to array                                                                                               
- * @size: Size of the array                                                                                                 
- * @action: argument to iterate array                                                                                       
- * Return: 0                                                                                                                
- */                                                                                                                         
-                                                                                                                            
-void array_iterator(int *array, size_t size, void (*action)(int))                                                           
-{                                                                                                                           
-        unsigned int i;                                                                                                     
-                                                                                                                            
-        if (action == NULL || array == NULL)                                                                                
-                return;                                                                                                     
-                                                                                                                            
-        for (i = 0; i < size; i++)
-		            action(array[i]);                                                                                          
-}        
+
+#include <stdlib.h>
+
+/**
+ * array_iterator -  function given as a parameter on each element of an array
+ * @array: A pointer to array
+ * @size: Size of the array
+ * @action: argumrnt to iterate array
+ * Return: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action == NULL || array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
