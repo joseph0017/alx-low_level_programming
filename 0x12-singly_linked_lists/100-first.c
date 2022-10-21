e5f64e3fd3ef3e42796a69a98f32ee2da9d0c1ab
joseph0017 <josephogiku96@gmail.com>
2022-10-20 20:43:22 -0700

    lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void msg_constructor(void) __attribute__((constructor));
 
 void msg_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
