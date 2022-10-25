6f8a5a00446985358c11a6ff871b93bd3631432a
joseph0017 <josephogiku96@gmail.com>
2022-10-25 15:34:48 -0700

    list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: head of a listint_t list
  * @idx: index
  * @n: value for node
